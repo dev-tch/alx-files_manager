3065daade858737a5cf851f9f37a8aaf49764a36
dev-tch <tijeni.chaabeni@gmail.com>
2024-06-14 21:00:07 +0000

    task0: implement class RedisClient

---
+++ utils/redis.js
@@ -0,0 +1,31 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => {
+      console.log(`Redis client not connected to the server: ${err.message}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const asyncGet = promisify(this.client.get).bind(this.client);
+    const reply = await asyncGet(key);
+    return reply;
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+const redisClient = new RedisClient();
+export default redisClient;
