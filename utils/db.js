a5f9a8562196d5e175e0ea0509a61234768b1a6c
dev-tch <tijeni.chaabeni@gmail.com>
2024-06-15 00:22:20 +0000

    task1: mongo v1

--- utils/db.js
+++ utils/db.js
@@ -1,12 +1,29 @@
-import MongoClient from 'mongodb';
+import { MongoClient } from 'mongodb';
+
+
+const h = process.env.DB_HOST || 'localhost';
+const p = process.env.DB_PORT || '27017';
+const dbName = process.env.connection || 'files_manager';
+const url = `mongodb://${h}:${p}`;
 
 class DBClient {
   constructor() {
-    const h = process.env.DB_HOST || 'localhost';
-    const p = process.env.DB_PORT || '27017'
-    const db = process.env.connection || 'files_manager'
-    const url = `mongodb://${h}:${p}/`
-
-
-
-
+    this.db = null;
+    // create client connexion 
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(dbName);
+      })
+      .catch((error) => {
+        console.log(`Failed to connect to MongoDb: ${error.message}`);
+        this.db = null;
+      });
+  }
+  
+  isAlive() {
+    return !!this.db;
+  }
+}
+const dbClient = new DBClient();
+export default dbClient;
