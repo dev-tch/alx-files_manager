f2a22786ec8d95b6af89b49f49debd9cd92aee73
dev-tch <tijeni.chaabeni@gmail.com>
2024-06-17 15:16:16 -0700

    task2: handle await of asynchronous methods

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,18 +2,22 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static getStatus(req, res) {
+  static async getStatus(req, res) {
+    const redisStatus = await redisClient.isAlive();
+    const dbStatus = await dbClient.isAlive();
     const resp = {
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
+      redis: redisStatus,
+      db: dbStatus,
     };
     return res.status(200).json(resp);
   }
 
-  static getStats(req, res) {
+  static async getStats(req, res) {
+    const nbUsers = await dbClient.nbUsers();
+    const nbFiles = await dbClient.nbFiles();
     const resp = {
-      users: dbClient.nbUsers(),
-      files: dbClient.nbFiles(),
+      users: nbUsers,
+      files: nbFiles,
     };
     return res.status(200).json(resp);
   }
